9af139c5e4f2963d763c599418cf816c62934e75
bearjam <sam.croce@gmail.com>
2020-07-18 12:30:04 -0400

    working code

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
+var bodyParser = require("body-parser");
 var express = require("express");
+var methodOverride = require("method-override");
 
-var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(process.cwd() + "/public"));
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+app.use("/", routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('The app is listening on port ' + port);
 });
